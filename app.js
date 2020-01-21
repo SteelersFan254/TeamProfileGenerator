74f95ae4a35e1ca9adc94dd7fba2b60c41126d7f
Ryan Borja <rcborja.school@gmail.com>
2020-01-21 02:12:43 -0600

    stuck for now

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const inquirer = require("inquirer");
+const employee = require("./lib/employee.js")
 
 // const fs = require("fs");
 // const generateHTML = require("generateHTML.js")
@@ -44,63 +45,65 @@ const managerQuestions = [
 inquirer
     .prompt(managerQuestions)
     .then(async function (managerResponse) {
+        let makeManager = new employee.Employee(managerResponse);
+
         // console.log(managerResponse)
         // console.log(managerResponse.engineers)
-        let NumOfEngineers = managerResponse.engineers;
-        let NumOfInterns = managerResponse.interns;
-        console.log(NumOfEngineers);
-        for (i = 0; i < NumOfEngineers; i++) {
-            await inquirer
-                .prompt([
-                    {
-                        type: "input",
-                        message: `What is engineer ${i + 1}'s name?`,
-                        name: "engineerName"
-                    },
-                    {
-                        type: "number",
-                        message: `What is engineer ${i + 1}'s ID?`,
-                        name: "engineerId"
-                    },
-                    {
-                        type: "input",
-                        message: `What is engineer ${i + 1}'s email?`,
-                        name: "engineerEmail"
-                    },
-                    {
-                        type: "input",
-                        message: `What is engineer ${i + 1}'s github username?`,
-                        name: "engineerGithub"
-                    }
-                ])
-                .then(function (engineerResponse) {
-
-                })
-        };
-        for (j = 0; j < NumOfInterns; j++) {
-            await inquirer
-                .prompt([
-                    {
-                        type: "input",
-                        message: `What is intern ${j + 1}'s name?`,
-                        name: "internName"
-                    },
-                    {
-                        type: "number",
-                        message: `What is intern ${j + 1}'s ID?`,
-                        name: "internId"
-                    },
-                    {
-                        type: "input",
-                        message: `What is intern ${j + 1}'s email?`,
-                        name: "internEmail"
-                    },
-                    {
-                        type: "input",
-                        message: `What is intern ${j + 1}'s school affiliation?`,
-                        name: "internSchool"
-                    }
-                ])
-        }
+    //     let NumOfEngineers = managerResponse.engineers;
+    //     let NumOfInterns = managerResponse.interns;
+    //     console.log(NumOfEngineers);
+    //     for (i = 0; i < NumOfEngineers; i++) {
+    //         await inquirer
+    //             .prompt([
+    //                 {
+    //                     type: "input",
+    //                     message: `What is engineer ${i + 1}'s name?`,
+    //                     name: "engineerName"
+    //                 },
+    //                 {
+    //                     type: "number",
+    //                     message: `What is engineer ${i + 1}'s ID?`,
+    //                     name: "engineerId"
+    //                 },
+    //                 {
+    //                     type: "input",
+    //                     message: `What is engineer ${i + 1}'s email?`,
+    //                     name: "engineerEmail"
+    //                 },
+    //                 {
+    //                     type: "input",
+    //                     message: `What is engineer ${i + 1}'s github username?`,
+    //                     name: "engineerGithub"
+    //                 }
+    //             ])
+    //             .then(function (engineerResponse) {
+    //                 console.log(engineerResponse)
+    //             })
+    //     };
+    //     for (j = 0; j < NumOfInterns; j++) {
+    //         await inquirer
+    //             .prompt([
+    //                 {
+    //                     type: "input",
+    //                     message: `What is intern ${j + 1}'s name?`,
+    //                     name: "internName"
+    //                 },
+    //                 {
+    //                     type: "number",
+    //                     message: `What is intern ${j + 1}'s ID?`,
+    //                     name: "internId"
+    //                 },
+    //                 {
+    //                     type: "input",
+    //                     message: `What is intern ${j + 1}'s email?`,
+    //                     name: "internEmail"
+    //                 },
+    //                 {
+    //                     type: "input",
+    //                     message: `What is intern ${j + 1}'s school affiliation?`,
+    //                     name: "internSchool"
+    //                 }
+    //             ])
+    //     }
     });
 // Use the Inquirer npm package to prompt the user for their email, id, and specific information based on their role with the company. For instance, an intern may provide their school, whereas an engineer may provide their GitHub username.
