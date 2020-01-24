0f57dad814952ea07a93cf9f339c0c085dda4c43
SteelersFan254 <rcborja.school@gmail.com>
2020-01-23 20:23:08 -0600

    finally got manager html working

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const Manager = require("./lib/manager.js")
 const Engineer = require("./lib/engineer.js")
 const Intern = require("./lib/intern.js")
 
-// const fs = require("fs");
+const fs = require("fs");
 // const generateHTML = require("generateHTML.js")
 const managerQuestions = [
     {
@@ -43,81 +43,93 @@ const managerQuestions = [
         // validate: confirmInternsNum()
     }
 ]
+async function makeTeam() {
+    inquirer
+        .prompt(managerQuestions)
+        .then(async function (managerResponse) {
+            var { managerName, managerId, managerEmail, managerOffice, engineers, interns } = managerResponse;
+            let makeManager = new Manager(managerName, managerId, managerEmail, managerOffice);
 
-inquirer
-    .prompt(managerQuestions)
-    .then(async function (managerResponse) {
-        var { managerName, managerId, managerEmail, managerOffice, engineers, interns } = managerResponse;
-        let makeManager = new Manager(managerName, managerId, managerEmail, managerOffice);
-        var engineerArray = [];
-        for (i = 0; i < engineers; i++) {
-            await inquirer
-                .prompt([
-                    {
-                        type: "input",
-                        message: `What is engineer ${i + 1}'s name?`,
-                        name: "engineerName"
-                    },
-                    {
-                        type: "number",
-                        message: `What is engineer ${i + 1}'s ID?`,
-                        name: "engineerId"
-                    },
-                    {
-                        type: "input",
-                        message: `What is engineer ${i + 1}'s email?`,
-                        name: "engineerEmail"
-                    },
-                    {
-                        type: "input",
-                        message: `What is engineer ${i + 1}'s github username?`,
-                        name: "engineerGithub"
-                    }
-                ])
-                .then(function (engineerResponse) {
+            var engineerArray = [];
+            for (i = 0; i < engineers; i++) {
+                await inquirer
+                    .prompt([
+                        {
+                            type: "input",
+                            message: `What is engineer ${i + 1}'s name?`,
+                            name: "engineerName"
+                        },
+                        {
+                            type: "number",
+                            message: `What is engineer ${i + 1}'s ID?`,
+                            name: "engineerId"
+                        },
+                        {
+                            type: "input",
+                            message: `What is engineer ${i + 1}'s email?`,
+                            name: "engineerEmail"
+                        },
+                        {
+                            type: "input",
+                            message: `What is engineer ${i + 1}'s github username?`,
+                            name: "engineerGithub"
+                        }
+                    ])
+                    .then(function (engineerResponse) {
 
-                    var { engineerName, engineerId, engineerEmail, engineerGitHub } = engineerResponse;
+                        var { engineerName, engineerId, engineerEmail, engineerGitHub } = engineerResponse;
 
-                    var makeEngineer = new Engineer(engineerName, engineerId, engineerEmail, engineerGitHub)
+                        var makeEngineer = new Engineer(engineerName, engineerId, engineerEmail, engineerGitHub)
 
-                    engineerArray.push(makeEngineer)
-                })
-        };
-        var internArray = []
-        for (j = 0; j < interns; j++) {
-            await inquirer
-                .prompt([
-                    {
-                        type: "input",
-                        message: `What is intern ${j + 1}'s name?`,
-                        name: "internName"
-                    },
-                    {
-                        type: "number",
-                        message: `What is intern ${j + 1}'s ID?`,
-                        name: "internId"
-                    },
-                    {
-                        type: "input",
-                        message: `What is intern ${j + 1}'s email?`,
-                        name: "internEmail"
-                    },
-                    {
-                        type: "input",
-                        message: `What is intern ${j + 1}'s school affiliation?`,
-                        name: "internSchool"
-                    }
-                ]).then(function(internResponse) {
-                    var { internName, internId, internEmail, internSchool } = internResponse;
+                        engineerArray.push(makeEngineer)
+                    })
+            };
+            var internArray = []
+            for (j = 0; j < interns; j++) {
+                await inquirer
+                    .prompt([
+                        {
+                            type: "input",
+                            message: `What is intern ${j + 1}'s name?`,
+                            name: "internName"
+                        },
+                        {
+                            type: "number",
+                            message: `What is intern ${j + 1}'s ID?`,
+                            name: "internId"
+                        },
+                        {
+                            type: "input",
+                            message: `What is intern ${j + 1}'s email?`,
+                            name: "internEmail"
+                        },
+                        {
+                            type: "input",
+                            message: `What is intern ${j + 1}'s school affiliation?`,
+                            name: "internSchool"
+                        }
+                    ]).then(function (internResponse) {
+                        var { internName, internId, internEmail, internSchool } = internResponse;
 
-                    var makeIntern = new Intern(internName, internId, internEmail, internSchool)
+                        var makeIntern = new Intern(internName, internId, internEmail, internSchool)
 
-                    internArray.push(makeIntern)
-                })
-        }
-        console.log(makeManager);
-        console.log(engineerArray);
-        console.log(internArray);
-        
-    });
-// Use the Inquirer npm package to prompt the user for their email, id, and specific information based on their role with the company. For instance, an intern may provide their school, whereas an engineer may provide their GitHub username.
+                        internArray.push(makeIntern)
+                    })
+            }
+            // console.log(makeManager);
+            // console.log(engineerArray);
+            // console.log(internArray);
+            var { name, id, email, officeNumber, role } = makeManager
+
+            let mgrCard = fs.readFileSync('./templates/manger.html', 'utf8');
+            mgrCard = mgrCard.replace('{{name}}', name);
+            mgrCard = mgrCard.replace('{{id}}', id);
+            mgrCard = mgrCard.replace('{{email}}', email);
+            mgrCard = mgrCard.replace('{{officeNumber}}', officeNumber);
+            mgrCard = mgrCard.replace('{{role}}', role);
+            console.log(mgrCard)
+
+        });
+
+}
+makeTeam();
