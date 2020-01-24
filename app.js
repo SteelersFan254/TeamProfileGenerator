1351c70ee8ad69e8698170e93d357cea4d105452
SteelersFan254 <rcborja.school@gmail.com>
2020-01-23 18:42:17 -0600

    got all employees info

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const inquirer = require("inquirer");
 const Manager = require("./lib/manager.js")
+const Engineer = require("./lib/engineer.js")
+const Intern = require("./lib/intern.js")
 
 // const fs = require("fs");
 // const generateHTML = require("generateHTML.js")
@@ -47,65 +49,75 @@ inquirer
     .then(async function (managerResponse) {
         var { managerName, managerId, managerEmail, managerOffice, engineers, interns } = managerResponse;
         let makeManager = new Manager(managerName, managerId, managerEmail, managerOffice);
-        console.log(makeManager, engineers, interns)
-        // console.log(makeManager);
-        // console.log(managerResponse)
-        // console.log(managerResponse.engineers)
-    //     let NumOfEngineers = managerResponse.engineers;
-    //     let NumOfInterns = managerResponse.interns;
-    //     console.log(NumOfEngineers);
-    //     for (i = 0; i < NumOfEngineers; i++) {
-    //         await inquirer
-    //             .prompt([
-    //                 {
-    //                     type: "input",
-    //                     message: `What is engineer ${i + 1}'s name?`,
-    //                     name: "engineerName"
-    //                 },
-    //                 {
-    //                     type: "number",
-    //                     message: `What is engineer ${i + 1}'s ID?`,
-    //                     name: "engineerId"
-    //                 },
-    //                 {
-    //                     type: "input",
-    //                     message: `What is engineer ${i + 1}'s email?`,
-    //                     name: "engineerEmail"
-    //                 },
-    //                 {
-    //                     type: "input",
-    //                     message: `What is engineer ${i + 1}'s github username?`,
-    //                     name: "engineerGithub"
-    //                 }
-    //             ])
-    //             .then(function (engineerResponse) {
-    //                 console.log(engineerResponse)
-    //             })
-    //     };
-    //     for (j = 0; j < NumOfInterns; j++) {
-    //         await inquirer
-    //             .prompt([
-    //                 {
-    //                     type: "input",
-    //                     message: `What is intern ${j + 1}'s name?`,
-    //                     name: "internName"
-    //                 },
-    //                 {
-    //                     type: "number",
-    //                     message: `What is intern ${j + 1}'s ID?`,
-    //                     name: "internId"
-    //                 },
-    //                 {
-    //                     type: "input",
-    //                     message: `What is intern ${j + 1}'s email?`,
-    //                     name: "internEmail"
-    //                 },
-    //                 {
-    //                     type: "input",
-    //                     message: `What is intern ${j + 1}'s school affiliation?`,
-    //                     name: "internSchool"
-    //                 }
-    //             ])
-    //     }
+        var engineerArray = [];
+        for (i = 0; i < engineers; i++) {
+            await inquirer
+                .prompt([
+                    {
+                        type: "input",
+                        message: `What is engineer ${i + 1}'s name?`,
+                        name: "engineerName"
+                    },
+                    {
+                        type: "number",
+                        message: `What is engineer ${i + 1}'s ID?`,
+                        name: "engineerId"
+                    },
+                    {
+                        type: "input",
+                        message: `What is engineer ${i + 1}'s email?`,
+                        name: "engineerEmail"
+                    },
+                    {
+                        type: "input",
+                        message: `What is engineer ${i + 1}'s github username?`,
+                        name: "engineerGithub"
+                    }
+                ])
+                .then(function (engineerResponse) {
+
+                    var { engineerName, engineerId, engineerEmail, engineerGitHub } = engineerResponse;
+
+                    var makeEngineer = new Engineer(engineerName, engineerId, engineerEmail, engineerGitHub)
+
+                    engineerArray.push(makeEngineer)
+                })
+        };
+        var internArray = []
+        for (j = 0; j < interns; j++) {
+            await inquirer
+                .prompt([
+                    {
+                        type: "input",
+                        message: `What is intern ${j + 1}'s name?`,
+                        name: "internName"
+                    },
+                    {
+                        type: "number",
+                        message: `What is intern ${j + 1}'s ID?`,
+                        name: "internId"
+                    },
+                    {
+                        type: "input",
+                        message: `What is intern ${j + 1}'s email?`,
+                        name: "internEmail"
+                    },
+                    {
+                        type: "input",
+                        message: `What is intern ${j + 1}'s school affiliation?`,
+                        name: "internSchool"
+                    }
+                ]).then(function(internResponse) {
+                    var { internName, internId, internEmail, internSchool } = internResponse;
+
+                    var makeIntern = new Intern(internName, internId, internEmail, internSchool)
+
+                    internArray.push(makeIntern)
+                })
+        }
+        console.log(makeManager);
+        console.log(engineerArray);
+        console.log(internArray);
+        
     });
 // Use the Inquirer npm package to prompt the user for their email, id, and specific information based on their role with the company. For instance, an intern may provide their school, whereas an engineer may provide their GitHub username.
